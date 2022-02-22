a98d9d0724819b1272b65efd0b1b608dd46e278c
Gianfranco <gianpa@gmail.com>
2022-02-22 12:02:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const { Telegraf } = require('telegraf');
+const path = require('path'); 
+// load env variables
+require('dotenv').config({ path: path.join(__dirname, '.env') });
+
+const bot = new Telegraf(process.env.BOTTOKEN);
+
+bot.start((ctx) => ctx.reply('Welcome 👩‍🌾'));
+
+bot.use(async (ctx, next) => {
+  console.log(ctx.message?.text);
+
+  const text = ctx.message?.text;
+  //   if (text === 'bye') {
+  //     await ctx.reply('Goodbye!', { reply_markup: keyBoard });
+  //   }
+  if (text === 'help') {
+    await ctx.reply('Available commands: hi, bye, help');
+  }
+  if (text === '/uptime') {
+    await ctx.reply('Uptime: ' + process.uptime());
+  }
+  // ctx.reply(ctx.message?.text ?? `OOPS, I don't understant.`)
+  return next;
+});
+
+
+bot.launch()
